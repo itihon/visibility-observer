26a1d855c11ba6980a8160480b16cc29cd5dc11f
itihon <kalabin.alexandr@gmail.com>
2025-03-23 22:11:15 +0500

    refactor(types): remove private properties from the generated *.d.ts file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,4 @@
 declare const _default: {
-    "__#1@#initialViewportRect": any;
-    "__#1@#lastViewportRect": any;
-    /** @type {Map<HTMLElement, "FULLY_VISIBLE" | "PARTLY_VISIBLE" | "INVISIBLE">} */
-    "__#1@#states": Map<HTMLElement, "FULLY_VISIBLE" | "PARTLY_VISIBLE" | "INVISIBLE">;
-    /** @type {Map<HTMLElement, IntersectionObserverEntry>} */
-    "__#1@#records": Map<HTMLElement, IntersectionObserverEntry>;
-    /**
-     * @param {IntersectionObserverEntry} entry
-     */
-    "__#1@#handleEntry"(entry: IntersectionObserverEntry): void;
-    "__#1@#updateStates"(): void;
     /**
      * @param {HTMLElement} target
      * @returns {"FULLY_VISIBLE" | "PARTLY_VISIBLE" | "INVISIBLE"}
